b5485c42d8eec6d896d19c3443bfa70224fde72c
akuol-nicole <a.anyar2@alustudent.com>
2026-09-20 13:14:11 -0700

    Improve permissions output formatting

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -2,7 +2,7 @@
 secure_data() {
 	echo "Securing active_logs directory..."
 	chmod 700 active_logs
-	echo "Permissions updated. New permissions for active_logs:"
+	echo "Directory permissions"
 	ls -l -d active_logs
 	# 700 = owner has read/write/execute, group and others have no access
 } 
