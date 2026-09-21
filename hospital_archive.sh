68f335a75d87fb2c74c0e5b165ca23963576029f
dapoladapo <d.oladapo@alustudent.com>
2026-09-21 11:03:06 -0700

    Completed hospital archive system

---
+++ hospital_archive.sh
@@ -0,0 +1,21 @@
+!/bin/bash 
+
+archiving_files() {
+        echo "Starting log archiving"
+        timestamp=$(date +"%Y%m%d_%H%M")
+
+        for file in active_logs/*; do
+                filename=$(basename "$file") #taking only the file without the path
+                nameonly="${filename%.*}" #takes the filename and extract only the name and leaves the extension
+                extensiononly="${filename##*.}" #takes only the extension and removes the filename
+
+                mv "$file" "archived_logs/${nameonly}_${timestamp}.${extensiononly}"
+                echo "Move completed"
+                touch "active_logs/$filename"
+                echo "New file created"
+        done
+
+        echo "Archiving process complete"
+}
+
+archiving_files
